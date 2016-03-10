465c069a9a8625b45a3ce9371f6ae984c167c825
samueru735 <samcontrol@hotmail.com>
2016-03-10 10:12:31 +0100

    some small changes, working

--- app/src/main/java/com/group4/ipd16/spirometer/BluetoothConnectActivity.java
+++ app/src/main/java/com/group4/ipd16/spirometer/BluetoothConnectActivity.java
@@ -1,16 +1,262 @@
 package com.group4.ipd16.spirometer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+import java.util.UUID;
 
 public class BluetoothConnectActivity extends AppCompatActivity {
+
+    private TextView myLabel;
+    private EditText myTextbox;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothSocket mmSocket;
+    private BluetoothDevice mmDevice;
+    private OutputStream mmOutputStream;
+    private InputStream mmInputStream;
+    private Thread workerThread;
+    private byte[] readBuffer;
+    private int readBufferPosition;
+    private int counter;
+    private volatile boolean stopWorker;
+    private String mac_address;
+    private Intent intent;
+    private TextView tvConnectInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_connect);
+
+        intent = this.getIntent();
+        mac_address = intent.getStringExtra("mac address");
+        Log.i("TAG", mac_address);
+        Button openButton = (Button)findViewById(R.id.open);
+        Button sendButton = (Button)findViewById(R.id.send);
+        Button closeButton = (Button)findViewById(R.id.close);
+        myLabel = (TextView)findViewById(R.id.label);
+        myTextbox = (EditText)findViewById(R.id.entry);
+        tvConnectInfo = (TextView)findViewById(R.id.tvConnectionInfo);
+
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try
+                {
+                    findBT();
+
+                    openBT();
+                }
+                catch (Exception ex) {
+                    Log.e("TAG", "Connection to " + mmDevice.getName() + " at "
+                            + mmDevice.getAddress() + " failed:" + ex.getMessage());}
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    sendData();
+                } catch (IOException ex) {
+                }
+            }
+        });
+
+        closeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                try
+                {
+                    closeBT();
+                }
+                catch (IOException ex) { }
+            }
+        });
+    }
+
+    void findBT()
+    {
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        /* if (btAdapter.isEnabled()) {
+
+            SharedPreferences prefs_btdev = getSharedPreferences("btdev", 0);
+            String btdevaddr=prefs_btdev.getString("btdevaddr","?");
+
+            if (mac_address != "?")
+            {
+                BluetoothDevice device = btAdapter.getRemoteDevice(mac_address);
+
+                UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // bluetooth serial port service
+                //UUID SERIAL_UUID = device.getUuids()[0].getUuid(); //if you don't know the UUID of the bluetooth device service, you can get it like this from android cache
+
+                //BluetoothSocket socket = null;
+
+                try {
+                    mmSocket = device.createRfcommSocketToServiceRecord(SERIAL_UUID);
+                } catch (Exception e) {Log.e("TAG","Error creating socket");}
+                try {
+                    mmSocket.connect();
+                    Log.i("TAG", "Connected");
+                    tvConnectInfo.setText("Connected");
+                } catch (IOException e) {
+                    Log.e("TAG",e.getMessage());
+                    try {
+                        Log.e("TAG","trying fallback...");
+
+                        mmSocket =(BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[] {int.class}).invoke(device,3);
+                        mmSocket.connect();
+
+                        Log.i("TAG","Connected through fallback");
+                    }
+                    catch (Exception e2) {
+                        Log.e("TAG", "Couldn't establish Bluetooth connection!" +  e2);
+                    }
+                }
+            }
+            else
+            {
+                Log.e("TAG","BT device not selected");
+            }
+        }*/
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+            myLabel.setText("No bluetooth adapter available");
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                //if(device.getName().equals("MattsBlueTooth"))
+                if(device.getAddress().equals(mac_address))
+                {
+                    mmDevice = device; Log.i("TAG","gotcha");
+                    break;
+                }
+            }
+        }
+        myLabel.setText("Bluetooth Device Found");
+
+    }
+
+    void openBT() throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        //mmSocket =(BluetoothSocket) mmDevice.getClass().getMethod("createRfcommSocket", new Class[] {int.class}).invoke(mmDevice,2);
+        Log.i("TAG","before");
+        mmSocket.connect();
+        Log.i("TAG", "after");
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+
+        beginListenForData();
+
+        myLabel.setText("Bluetooth Opened");
+    }
+
+    void beginListenForData()
+    {
+       final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            myLabel.setText(data);
+                                            Log.i("TAG", "data received");
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData() throws IOException
+    {
+        String msg = myTextbox.getText().toString();
+        msg += "\n";
+        mmOutputStream.write(msg.getBytes());
+        myLabel.setText("Data Sent");
+    }
+
+    void closeBT() throws IOException
+    {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+        myLabel.setText("Bluetooth Closed");
     }
 
     @Override
