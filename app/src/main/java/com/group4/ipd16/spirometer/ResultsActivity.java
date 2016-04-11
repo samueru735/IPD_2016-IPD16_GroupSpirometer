e4b5183175cb74d01c480728698a5ce65742f367
samueru735 <samcontrol@hotmail.com>
2016-04-11 10:52:33 +0200

    Added receiving data from home activity

--- app/src/main/java/com/group4/ipd16/spirometer/ResultsActivity.java
+++ app/src/main/java/com/group4/ipd16/spirometer/ResultsActivity.java
@@ -1,16 +1,57 @@
 package com.group4.ipd16.spirometer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ResultsActivity extends AppCompatActivity {
+    BluetoothConnection btConn;
+    Intent intent;
+    private TextView tvResult, tvSentData, tvConnStatus;
+    private double[] resultsArray;
+    private List<Double> listResults = new ArrayList<Double>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Bundle b = this.getIntent().getExtras();
+        try{
+            resultsArray =  b.getDoubleArray("results");
+            Double[] doubleArray = ArrayUtils.toObject(resultsArray);
+            listResults = new LinkedList<>(Arrays.asList(doubleArray));
+            Log.i("TAG", "Lisssstresults: " + listResults.toString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        MyMath.FilterResults(listResults);
+
+        tvConnStatus = (TextView)findViewById(R.id.tvConnStatus);
+        tvResult = (TextView)findViewById(R.id.tvResult);
+        tvSentData = (TextView)findViewById(R.id.tvSentData);
+
+        tvResult.setText(listResults.toString()); //Arrays.toString(resultsArray));
+
+      /*  btConn.Result(tvResult);
+        btConn.ListResults(listResult);
+        btConn.SentData(tvSentData);
+
+        try {
+            btConn.sendData("start");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 
     @Override
