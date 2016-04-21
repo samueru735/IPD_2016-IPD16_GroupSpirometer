ccc5c250532955c4333498f9482638c8cc8905ca
samueru735 <samcontrol@hotmail.com>
2016-04-21 22:12:26 +0200

    fixed db crash

--- app/src/main/java/com/group4/ipd16/spirometer/LoginActivity.java
+++ app/src/main/java/com/group4/ipd16/spirometer/LoginActivity.java
@@ -1,22 +1,12 @@
 package com.group4.ipd16.spirometer;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,10 +64,22 @@ public class LoginActivity extends BaseActivity {
         userSpinner = (Spinner)findViewById(R.id.userSpinner);
         List<String> userList = new ArrayList<String>();
         DatabaseHandler db = new DatabaseHandler(this);
-        List<User> users = db.getAllUsers();
-        for (User user : users) {
-            userList.add(user.getFirst_name());
+        List<User> users = new ArrayList<>();
+
+        // risky try catch, needs to be updated
+        try{
+            users = db.getAllUsers();
         }
+        catch (Exception ex){
+            LoginActivity.this.deleteDatabase("userInfo");
+        }
+
+        if(users != null ){
+            for (User user : users) {
+                userList.add(user.getFirst_name());
+            }
+        }
+
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, userList);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
