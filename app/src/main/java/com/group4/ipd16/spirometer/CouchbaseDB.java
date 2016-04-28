80483ec31847e869c664b2eb4849470a1a1896db
samueru735 <samcontrol@hotmail.com>
2016-04-28 23:33:03 +0200

    added change profile

--- app/src/main/java/com/group4/ipd16/spirometer/CouchbaseDB.java
+++ app/src/main/java/com/group4/ipd16/spirometer/CouchbaseDB.java
@@ -12,6 +12,7 @@ import com.couchbase.lite.Mapper;
 import com.couchbase.lite.Query;
 import com.couchbase.lite.QueryEnumerator;
 import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.UnsavedRevision;
 import com.couchbase.lite.View;
 import com.couchbase.lite.android.AndroidContext;
 
@@ -24,22 +25,45 @@ import java.util.Map;
 /**
  * Created by Samuel on 24/04/2016.
  */
-public class CouchbaseDB extends BaseActivity {
-    private Manager manager = null;
-    private Database database = null;
+public class CouchbaseDB  {
+    private static final String USER_ID = "user_id";
+    private static Manager manager = null;
+    private static Database database = null;
     private static final String DB_NAME = "spiro_db";
     private static final String TAG = "dbEvents";
-    private Context context;
+    private static Context context;
     private View usersByNameView = null;
+    private String userId;
+    private User currentUser;
 
-    public CouchbaseDB( Context context){
-        this.context = context;
+private  static CouchbaseDB spriroDB = new CouchbaseDB();
+  //  @Override
+ /*   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         setUpCouchbaseLiteDB();
+       // context = getApplicationContext();
+    }*/
+
+    public static CouchbaseDB getSpiroDB(){
+        if(spriroDB != null)
+            return spriroDB;
+        else {
+            return new CouchbaseDB();
+            //return new CouchbaseDB(ProfileActivity.this);
+        }
     }
 
-    private void setUpCouchbaseLiteDB() {
+    public CouchbaseDB()// Context context)
+     {
+        // this.context = getApplicationContext();
+    //    this.context = context;
+      //  setUpCouchbaseLiteDB();
+    }
+
+    public void setUpCouchbaseLiteDB() {
         try{
-            manager = new Manager( new AndroidContext(context) , Manager.DEFAULT_OPTIONS);
+            manager = new Manager( new AndroidContext(context), Manager.DEFAULT_OPTIONS);
             database = manager.getDatabase(DB_NAME);
         } catch (CouchbaseLiteException e) {
             e.printStackTrace(); return;
@@ -58,6 +82,33 @@ public class CouchbaseDB extends BaseActivity {
         } catch (CouchbaseLiteException e) {
             e.printStackTrace();
         }
+    }
+    public void setCurrentUser(Document user) {
+        Log.i(TAG, "Set user: " + user.getProperties());
+        //int id, String first_name, String last_name, String doctor_email, int age, int height, int weight, String gender, String ethnicity
+        currentUser = new User(userId, user.getProperty("first_name").toString(), user.getProperty("last_name").toString(), user.getProperty("mail_doctor").toString(),
+                user.getProperty("age").toString(), user.getProperty("height").toString(), user.getProperty("weight").toString(), user.getProperty("gender").toString(), user.getProperty("ethnicity").toString());
+    }
+    public User getCurrentUser(){
+        return currentUser;
+    }
+    public User getUserById(String userId){
+        Log.i(TAG, "user id: " + userId);
+        Document userDocument = database.getDocument(userId);
+        //return userDocument;
+        setCurrentUser(userDocument);
+        return getCurrentUser();
+    }
+    public void deleteDoc(String userid) {
+        Document doc = database.getDocument(userid);
+        try {
+            doc.delete();
+        } catch (CouchbaseLiteException e) {
+            e.printStackTrace();
+        }
+    }
+    public void cleanDB(){
+
     }
 
     protected void CreateDocument(Map<String, Object> userMap){
@@ -78,12 +129,16 @@ public class CouchbaseDB extends BaseActivity {
         List<String> users = new ArrayList<String>();
         Query query = database.createAllDocumentsQuery();
         query.setAllDocsMode(Query.AllDocsMode.ALL_DOCS);
+
+
         try {
             QueryEnumerator result = query.run();
             for (Iterator<QueryRow> it = result; it.hasNext();){
                 QueryRow row = it.next();
+                Document doc = row.getDocument();
                 Log.i(TAG, String.valueOf(row.getDocument().getProperties()));
-                users.add( String.valueOf(row.getDocument().getProperty("first_name")) + " " + String.valueOf(row.getDocument().getProperty("last_name")));
+                users.add(String.valueOf(row.getDocument().getProperty("first_name")) + " " + String.valueOf(row.getDocument().getProperty("last_name")));
+                getUserById(doc.getId());
             }
 
         } catch (CouchbaseLiteException e) {
@@ -93,16 +148,21 @@ public class CouchbaseDB extends BaseActivity {
         return users;
     }
 
-    private void updateDoc(String documentId, Map<String, Object> updatedProperties ){
-        Document document = database.getDocument(documentId);
-        try{
-            // update document with more data
-            //Map<String, Object> updatedProperties = new HashMap<String, Object>();
-            updatedProperties.putAll(document.getProperties());
-            // save to couchbase local couchbase Lite DB
-            document.putProperties(updatedProperties);
+    public void updateDoc(String documentId, final Map<String, Object> updatedProperties ){
+        Document doc = database.getDocument(documentId);
+        try {
+            doc.update(new Document.DocumentUpdater() {
+                @Override
+                public boolean update(UnsavedRevision newRevision) {
+                    Map<String, Object> properties =  newRevision.getUserProperties();
+                    Log.i(TAG, "User properties: " + properties);
+                    properties.putAll(updatedProperties);
+                    newRevision.setUserProperties(properties);
+                    return true;
+                }
+            });
         } catch (CouchbaseLiteException e) {
-            Log.e(TAG, "Error putting ", e);
+            e.printStackTrace();
         }
     }
     private View getView(String name) {
@@ -151,11 +211,20 @@ public class CouchbaseDB extends BaseActivity {
                 String docId = (String) row.getSourceDocumentId();
                 Log.i(TAG, "DocID: " + docId);
                 Log.i(TAG, "Given password: " + password);
-                Log.i(TAG, "Password: " + row.getDocument().getProperty("password").toString());
-                if(password.equals(row.getDocument().getProperty("password").toString())){
-                    Log.i(TAG, "password found");
-                    return docId;
+                try{
+                    Log.i(TAG, "Password: " + row.getDocument().getProperty("password").toString());
+                    if(password.equals(row.getDocument().getProperty("password").toString())){
+                        userId = docId;
+                        setCurrentUser(row.getDocument());
+                        Log.i(TAG, "password found");
+                        return docId;
+                    }
                 }
+                catch (Exception e){
+                    row.getDocument().delete();
+                    return null;
+                }
+
             }
             return "Access denied";
         } catch (CouchbaseLiteException e) {
@@ -163,6 +232,14 @@ public class CouchbaseDB extends BaseActivity {
         }
         return null;
     }
+
+  /*  private void saveUserId(String userId){
+    SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(USER_ID, MODE_PRIVATE);
+    //SharedPreferences sharedPref = getSharedPreferences(USER_ID,MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPref.edit();
+    editor.putString("user_id", userId);
+    editor.commit();
+    }*/
 
     public Database getDatabaseInstance() throws CouchbaseLiteException{
         if((this.database == null) & (this.manager != null)){
@@ -173,9 +250,15 @@ public class CouchbaseDB extends BaseActivity {
 
     public Manager getManagerInstance() throws IOException {
         if (manager == null) {
-            manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
+            manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS);
         }
         return manager;
     }
+
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
 
 }
---
+++ app/src/main/java/com/group4/ipd16/spirometer/ProfileActivity.java
@@ -0,0 +1,190 @@
+package com.group4.ipd16.spirometer;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.couchbase.lite.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProfileActivity extends BaseActivity {
+    private String userid;
+    private String gender;
+    DatabaseHandler db = new DatabaseHandler(this);
+    private CouchbaseDB spiroDB;
+    private EditText editFirstName, editLastName, editAge, editWeight, editHeight, editDoctor;
+    private EditText[] inputFields;
+    private RadioButton radioMale, radioFemale;
+    private Spinner ethnicitySpinner;
+    private List<String> ethnicityList;
+    private User user;
+    private Document dUser;
+    //private String userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLayoutInflater().inflate(R.layout.activity_profile, frameLayout);
+
+        Button btnSaveProfile = (Button) findViewById(R.id.btnSaveProfile);
+        Button btnDeleteProfile = (Button)findViewById(R.id.btnDeleteProfile);
+        editFirstName = (EditText) findViewById(R.id.editFirstName);
+        editLastName = (EditText) findViewById(R.id.editLastName);
+        //editPassword = (EditText) findViewById(R.id.editPassword);
+        editDoctor = (EditText) findViewById(R.id.editDoctor);
+        editAge = (EditText) findViewById(R.id.editAge);
+        editHeight = (EditText) findViewById(R.id.editHeight);
+        editWeight = (EditText) findViewById(R.id.editWeight);
+
+        inputFields = new EditText[]{editFirstName, editLastName, editDoctor, editAge,editHeight, editWeight};
+
+        radioMale = (RadioButton) findViewById(R.id.radioButtonMale);
+        radioFemale = (RadioButton) findViewById(R.id.radioButtonFemale);
+
+        ethnicitySpinner = (Spinner) findViewById(R.id.spinner);
+        ethnicityList = new ArrayList<String>();
+        ethnicityList.add("Caucasian");
+        ethnicityList.add("Asian");
+        ethnicityList.add("Hispanic");
+        ethnicityList.add("African American");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ethnicityList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ethnicitySpinner.setAdapter(dataAdapter);
+
+        Intent i = getIntent();
+        userid = i.getStringExtra("user_id");
+        spiroDB = CouchbaseDB.getSpiroDB();
+
+        List<String>testList = spiroDB.getAllUsers();
+        Log.i("TAG", "Testlist contains: " + testList.toString());
+
+        //userid = spiroDB.getUserId();
+        Log.i("TAG", "Current user id: " + userid);
+
+
+        try{
+            user = spiroDB.getUserById(userid);
+            Log.i("TAG", "Current user age: " + user.getAge());
+            FilloutForm();
+        }
+        catch (Exception e){
+            Log.i("TAG", "Error occured: ", e);
+        }
+
+
+        btnSaveProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if( controlEmptyInputFields() == false){
+                    Toast.makeText(ProfileActivity.this, "Fill out correctly!", Toast.LENGTH_LONG).show();
+                }
+                else{
+
+                    if (radioMale.isChecked()) {
+                        gender = "male";
+                    }
+                    else if (radioFemale.isChecked()) {
+                        gender = "female";
+                    }
+                    Map<String, Object> profile = new HashMap<String, Object>();
+                    profile.put("first_name", editFirstName.getText().toString());
+                    profile.put("last_name", editLastName.getText().toString());
+                    profile.put("mail_doctor", editDoctor.getText().toString());
+                    profile.put("age", editAge.getText().toString());
+                    profile.put("height", editHeight.getText().toString());
+                    profile.put("weight", editWeight.getText().toString());
+                    profile.put("ethnicity", ethnicitySpinner.getSelectedItem().toString());
+                    profile.put("gender", gender);
+                    //profile.put("password", editPassword.getText().toString());
+                    spiroDB.updateDoc(userid, profile);
+                    Toast.makeText(ProfileActivity.this, "Profile saved", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(ProfileActivity.this, LoginActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+        btnDeleteProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spiroDB.deleteDoc(userid);
+                Toast.makeText(ProfileActivity.this, "Profile deleted", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void FilloutForm() {
+        editFirstName.setText("" +user.getFirst_name(), TextView.BufferType.EDITABLE);
+        editLastName.setText(user.getLast_name().toString());
+        editAge.setText(Integer.parseInt(user.getAge() + "") + "");
+        editHeight.setText(Integer.parseInt(""+user.getHeight())+"");
+        editWeight.setText(Integer.parseInt("" + user.getWeight()) + "");
+        editDoctor.setText(user.getDoctor_email());
+        if(user.getGender().equals("male"))
+            radioMale.setChecked(true);
+        else
+            radioFemale.setChecked(true);
+
+        ethnicitySpinner.setSelection(ethnicityList.indexOf(user.getEthnicity()));
+    }
+
+   protected CouchbaseDB getSpiroDB(){
+       return CouchbaseDB.getSpiroDB();
+      // if(spiroDB != null)
+       //     return spiroDB;
+       //else
+       //   return new CouchbaseDB(ProfileActivity.this);
+   }
+
+    private boolean controlEmptyInputFields() {
+        boolean pass = true;
+        for(int i = 0; i < inputFields.length; i++){
+            if(inputFields[i].getText().toString().equals("")){
+                inputFields[i].setBackgroundResource(R.drawable.edittextborder);
+                pass = false;
+            }
+            else{
+                inputFields[i].setBackgroundColor(Color.WHITE);
+            }
+        }
+        return pass;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
