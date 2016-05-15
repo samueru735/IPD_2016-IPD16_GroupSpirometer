eda0073d183a8d896a74d2bb03787818345a0e92
samueru735 <samcontrol@hotmail.com>
2016-05-16 00:40:32 +0200

    added individual graphs + listview for previous measurements

--- app/src/main/java/com/group4/ipd16/spirometer/HistoryActivity.java
+++ app/src/main/java/com/group4/ipd16/spirometer/HistoryActivity.java
@@ -1,74 +1,102 @@
 package com.group4.ipd16.spirometer;
 
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.OnDataPointTapListener;
-import com.jjoe64.graphview.series.PointsGraphSeries;
 import com.jjoe64.graphview.series.Series;
 
-import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 public class HistoryActivity extends BaseActivity {
-
-    public ListView list;
-
+    private List<Map<String, Object>> resultList = new ArrayList<>();
+    private List<Double>listResults = new ArrayList<>();
+    private List<Date> listViewResultDates = new ArrayList<>();
+    private List<String> listViewResultStringDates = new ArrayList<>();
+    private ListView lvResults;
+    private ArrayAdapter<String> arrayAdapter;
+    private TextView tvLegendX, tvLegendY, tvPercent;
+    private Double fvc, fev1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_history);
         getLayoutInflater().inflate(R.layout.activity_history, frameLayout);
+        tvLegendX = (TextView)findViewById(R.id.tvLegendX);
+        tvLegendY = (TextView)findViewById(R.id.tvLegendY);
+        tvPercent = (TextView)findViewById(R.id.tvPercent);
+        lvResults = (ListView)findViewById(R.id.lvResults);
         drawerList.setItemChecked(position, true);
+        resultList = CouchbaseDB.getSpiroDB().getResultsFromId();
+        Log.i("TAG", "Resultlist from user: " + resultList);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        for(Map result : resultList){
+            try {
+                listViewResultDates.add(df.parse(result.get("date").toString()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        BarGraphSeries<DataPoint> bar_series = new BarGraphSeries<DataPoint>();
 
-        list = (ListView)findViewById(R.id.listDates);
+        Collections.sort(listViewResultDates);
+        int counter= 0;
+        int dayOfMonth = 0;
+        for(Date date : listViewResultDates){
+            String formattedDate = df.format(date);
+            String substringDate = String.format(formattedDate).substring(0, 2);
+            dayOfMonth = Integer.parseInt(substringDate);
+            double fvc;
+            listViewResultStringDates.add(formattedDate);
+            for (Map result : resultList){
+                if(result.get("date").toString().equals(formattedDate)) {
+                    fvc = Double.parseDouble(result.get("fvc").toString());
+                    bar_series.appendData(new DataPoint(dayOfMonth, fvc),true,7);
+                    counter++;
+                }
+            }
 
-        String[] testDates = new String[]{"10 Jan 2016", "3 Okt 2015", "12 Sept 2013"};
+        }
+        Log.i("TAG", "Listview dates: " + listViewResultStringDates);
+        arrayAdapter = new ArrayAdapter(HistoryActivity.this,android.R.layout.simple_selectable_list_item);
+        arrayAdapter.addAll(listViewResultStringDates);
+        lvResults.setAdapter(arrayAdapter);
 
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, testDates);
-
-        list.setAdapter(adapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int itemPosition = position;
-                String itemValue = (String)list.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), "position" + itemPosition + "Listitem" + itemValue, Toast.LENGTH_LONG).show();
-
+                String dateString = ((TextView) view).getText().toString();
+                Log.i("TAG", "Clicked date: " + dateString);
+                PutDataInListResults(dateString);
+                DrawResultGraph();
             }
         });
 
+
         GraphView bar_graph = (GraphView)findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> bar_series = new BarGraphSeries<DataPoint>(new DataPoint[]{
-                new DataPoint(0, -2),
-                new DataPoint(1, 7),
-                new DataPoint(2, 4),
-                new DataPoint(3, 5),
-                new DataPoint(5, 2),
-                new DataPoint(4, 8)
-        });
+
+
         bar_graph.addSeries(bar_series);
         //kleur
 
@@ -78,9 +106,8 @@ public class HistoryActivity extends BaseActivity {
                 return Color.BLACK;
             }
         });
-
         //space tussen bars
-        bar_series.setSpacing(50);
+        bar_series.setSpacing(5);
         // waarden op top
         bar_series.setDrawValuesOnTop(true);
         bar_series.setValuesOnTopColor(Color.BLACK);
@@ -88,11 +115,9 @@ public class HistoryActivity extends BaseActivity {
 
         //scrolling
         bar_graph.getViewport().setScrollable(true);
-        bar_graph.getViewport().setXAxisBoundsManual(true);
-        bar_graph.getViewport().setMinX(0.5);
-        bar_graph.getViewport().setMaxX(3.5);
-        bar_graph.getViewport().setScrollable(true);
-        bar_graph.getViewport().scrollToEnd();
+        bar_graph.getViewport().setXAxisBoundsManual(false);
+        bar_graph.getViewport().setMinX(dayOfMonth-counter);
+        bar_graph.getViewport().setMaxX(dayOfMonth-counter+4);
         // tap info
         bar_series.setOnDataPointTapListener(new OnDataPointTapListener() {
             @Override
@@ -101,4 +126,45 @@ public class HistoryActivity extends BaseActivity {
             }
         });
     }
+
+    private void PutDataInListResults(String dateString) {
+        for (Map result : resultList) {
+            if (result.get("date").toString().equals(dateString)) {
+                listResults = (List<Double>) result.get("data");
+                fvc = (Double) result.get("fvc");
+                fev1 = (Double) result.get("fev1");
+            }
+        }
+    }
+
+    private void DrawResultGraph() {
+
+        GraphView graph = (GraphView)findViewById(R.id.graph);
+        tvLegendY.setText("FVC = " + fvc);
+        tvLegendX.setText("FEV1 = " + fev1);
+        tvPercent.setText("FEV1/FVC = " + MyMath.round(fev1/fvc*100, 1) + "%");
+        graph.removeAllSeries();
+        DataPoint[]resultDataPoints = new DataPoint[listResults.size()];
+        for(int i = 0; i < listResults.size(); i++)
+        {
+            resultDataPoints[i] = new DataPoint(i, listResults.get(i));
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(resultDataPoints);
+        graph.addSeries(series);
+
+        series.setTitle("(FVC, forced vital capacity) in L/s");
+
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(1);
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(HistoryActivity.this, "Series: On Data Point clicked: " + dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
